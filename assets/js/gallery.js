9d80fc1d0af5173d534bd6c20ae925013a23b9f2
David <nizz3hx@protonmail.com>
2025-07-29 16:19:51 +0100

    added pagination, 15 images per row page.

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -65,43 +65,85 @@ const galleryItems = [
   { category: 'posters', path: './images/art/posters/IMG_2343.PNG' },
 ];
 
-let galleryPush = document.querySelector(".drawing");
+const itemsPerPage = 15;
+let galleryContainer = document.querySelector(".drawing");
+let paginationContainer = document.querySelector(".pageNumbers")
+let filterListItems = document.querySelectorAll(".list")
 
-
-function renderGallery(galleryItems){
-    let galleryString = '';
-    
-    galleryItems.forEach(item => {
-        galleryString += (`<div class="drawingBox" data-item="${item.category}"><img src="${item.path}"></div>`)
-    }
-    )
-
-    galleryPush.innerHTML = galleryString
-
+let state = {
+    currentItems: galleryItems,
+    currentPage: 1
 }
 
-renderGallery(galleryItems)
+function updateGalView(){
+    const startIndex = (state.currentPage - 1) * itemsPerPage
+    const endIndex = startIndex + itemsPerPage
+    const itemsToShow = state.currentItems.slice(startIndex, endIndex)
 
-const filterListItems = document.querySelectorAll(".list");
-let galleryImages = document.querySelectorAll(".drawingBox");
+    const pagesTotal = Math.ceil(state.currentItems.length / itemsPerPage)
 
+    renderGallery(itemsToShow, galleryContainer)
+    renderPagination(paginationContainer, pagesTotal, state.currentPage)
+}
 
-filterListItems.forEach(item => {
-    item.addEventListener('click', () => {
-        filterListItems.forEach(li => li.classList.remove('active'))
-        item.classList.add('active');
+function renderGallery(itemsToShow, galleryContainer) {
+  let galleryString = "";
 
-        const dataFilter = item.getAttribute("data-filter")
+  itemsToShow.forEach((item) => {
+    galleryString += `<div class="drawingBox" data-item="${item.category}"><img src="${item.path}"></div>`;
+  });
 
-        galleryImages.forEach(image => {
-            const itemCategory = image.getAttribute('data-item')
+  galleryContainer.innerHTML = galleryString;
+}
 
-        if (dataFilter === 'all' || dataFilter === itemCategory){
-            image.classList.remove('hide') 
-        } else {
-            image.classList.add('hide')
+function renderPagination(paginationContainer, pagesTotal, currentPage){
+    paginationContainer.innerHTML = ''
+    let pages = ''
+
+    for(let i = 1; i <= pagesTotal; i++){
+        const isActive = i === currentPage ? 'active' : ''
+        pages += (`<a href="#two" class="page-link ${isActive}" data-page="${i}">${i}</a>`)
+    }
+
+    paginationContainer.innerHTML = pages
+}
+
+function filterListeners(){
+    filterListItems.forEach(item => {
+        item.addEventListener('click', () => {
+            filterListItems.forEach(li => li.classList.remove('active'))
+            item.classList.add('active')
+
+            const dataFilter = item.getAttribute('data-filter')
+
+            if (dataFilter === 'all'){
+                state.currentItems = galleryItems
+            } else {
+                state.currentItems = galleryItems.filter(galleryItem => {
+                   return galleryItem.category === dataFilter
+                })
+            }
+            state.currentPage = 1
+            updateGalView()
+        })
+    })
+}
+
+function paginationListeners(){
+    paginationContainer.addEventListener('click', (event) => {
+        if(event.target.matches('.page-link')) {
+            event.preventDefault()
+
+            const pageNumber = parseInt(event.target.getAttribute('data-page'), 10)
+
+            state.currentPage = pageNumber
+            updateGalView()
         }
     })
-})
-  });
+}
+
+filterListeners()
+paginationListeners()
+
+updateGalView()
 })
